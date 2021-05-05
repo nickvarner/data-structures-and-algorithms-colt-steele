29461589d017d3bdb57928ee2b75474929652899
[nick varner] <nick.varner@gmail.com>
2021-05-05 13:42:54 -0700

    finished multiple pointers

--- 01-problem-solving-patterns/multiple_pointers.js
+++ 01-problem-solving-patterns/multiple_pointers.js
@@ -44,3 +44,49 @@ const countUniqueValues = (arr) => {
 	}
 	return Object.keys(charMap).length;
 };
+// multiple pointer solution
+// try to explain this here
+// i
+// [1, 1, 1, 2, 3, 3, 4, 4, 5, 6]
+//     j
+// i and j are the same, so were going to leave i there and move j forward
+// i
+// [1, 1, 1, 2, 3, 3, 4, 4, 5, 6]
+//        j
+// i and j are the same, so were going to leave i there and move j forward
+// i
+// [1, 1, 1, 2, 3, 3, 4, 4, 5, 6]
+// 	      j
+// j acts as a scout, i and j are now different or unique, so we'll move i forward, swap j's value to i and move j forward
+//     i
+// [1, 2, 1, 2, 3, 3, 4, 4, 5, 6]
+// 	      j
+// next one looks like this (unique/swap)
+//        i
+// [1, 2, 3, 2, 3, 3, 4, 4, 5, 6]
+// 	            j
+// by the end we've moved i for every unique pair and this is what it looks like
+// so i's index (plus one to include 0) will be the number of unique values
+//                 i
+// [1, 2, 3, 4, 5, 6, 4, 4, 5, 6]
+//                             j
+
+print('you need to work on this one, you get the for loop backward in regard to the if statement');
+print(
+	'remember if they ARE equal we want j to move up, so we need to create the conditional statement only when they are not equal by moving i up and swapping their values'
+);
+print(
+	'this wont always be in numerical order. we just have to swap them because we need them to not be equal to the loop can move forward'
+);
+print('and the index is basically our counter. this is more confusing way to solve it than above imo');
+function countUniqueValues2 (arr) {
+	if (arr.length === 0) return 0;
+	let i = 0;
+	for (let j = 1; j < arr.length; j++) {
+		if (arr[i] !== arr[j]) {
+			i++;
+			arr[i] = arr[j];
+		}
+	}
+	return i + 1;
+}
